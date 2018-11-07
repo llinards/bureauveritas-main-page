405535b069061dcc5485923913e4ae3c7901420a
Linards Lazdiņš <linards_lazdins@hotmail.com>
2018-11-07 13:00:29 +0200

    final release

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("Connected!");
+$(document).ready(function () {
+
+    $("#one .container-fluid .row .btn-col").vegas({
+        slides: [{
+                src: "/img/bg.jpg"
+            },
+            {
+                src: "/img/bg2.jpg"
+            },
+            {
+                src: "/img/bg3.jpg"
+            }
+        ]
+    });
+
+});
